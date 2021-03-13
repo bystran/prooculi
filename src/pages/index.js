60b6f72f9b8327e5258eca75d15606af91177d44
Adam Bystran <bystran.adam.21@gmail.com>
2021-03-13 04:55:27 +0000

    Removes banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {useStaticQuery} from "gatsby"
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,34 +13,16 @@ import Contact from '../components/sections/domov/contatct';
 import Facebook from '../components/sections/domov/facebook';
 import LazyLoad from 'react-lazyload'
 import Spinner from 'react-spinners/CircleLoader'
-import OverlayBanner from "../components/overlayBanner/overlayBanner";
 
-const query = graphql`
-  {
-    file(relativePath: {eq: "banner-lady.png"}) {
-      img:childImageSharp  {
-        fluid (maxWidth: 1400) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`
+
 
 
 const IndexPage = () => {
-  const {file:{img:{fluid}}} = useStaticQuery(query);
 
-  const combined_bg = [
-      `linear-gradient(0deg, #90e0ef 0%, rgba(108, 189, 208, 1) 14.11%, rgba(108, 189, 208, 0.93) 35.27%, rgba(108, 189, 208, 0.1) 70%, rgba(108, 189, 208, 0.07) 100%)`,    
-      fluid
-  ]
   return (
     <Layout>
       <SEO title="Domov" />
-      <OverlayBanner 
-        background={combined_bg}
-      />
+
 
       <Home1 />
       <Home2 />
